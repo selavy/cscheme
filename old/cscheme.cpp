b274995cd4ceb53da9f8d08e8aba2244220ad8b1
Peter Lesslie <pclesslie@gmail.com>
2019-05-24 10:15:18 -0500

    Second attempt

---
+++ old/cscheme.cpp
@@ -0,0 +1,251 @@
+#include <cstdio>
+#include <cassert>
+#include <list>
+#include <vector>
+#include <sstream>
+#include <initializer_list>
+#include "value.h" // TODO: this is evil, must be included _before_ lex.cpp
+#include "lex.cpp"
+
+enum Status { OK, ERROR, DONE };
+
+enum
+{
+    F_PLUS,
+    F_MINUS,
+    NUM_BUILTINS,
+};
+
+struct Tokens
+{
+    Tokens(FILE* f) noexcept : input(f), token(Token::ERROR) {}
+
+    void next()
+    {
+        if (token == Token::FINISHED) {
+            return;
+        }
+        token = lex(input, &value);
+        if (token == Token::ERROR) {
+            throw std::runtime_error{"error parsing input"};
+        }
+    }
+
+    void expect(Token t)
+    {
+        if (token != t) {
+            // TODO: need printf-style formatter for exceptions
+            throw std::runtime_error{"unexpected token"};
+        }
+        next();
+    }
+
+    bool match(std::initializer_list<Token> tokens)
+    {
+        for (auto t : tokens) {
+            if (match(t)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool match(Token t)
+    {
+        if (token == t) {
+            next();
+            return true;
+        }
+        return false;
+    }
+
+    Value* peek() {
+        return value;
+    }
+
+    Input  input;
+    Token  token;
+    Value* value;
+};
+
+Value* mklist(const std::vector<Value*>& values)
+{
+    if (values.empty()) {
+        return NIL;
+    }
+    auto it = values.begin();
+    Value* result = mkpair(*it++, NIL);
+    Value* cur = result;
+    for (; it != values.end(); ++it) {
+        cur->p.cdr = mkpair(*it, NIL);
+        cur = cur->p.cdr;
+    }
+    return result;
+}
+
+Status eval(Tokens& tokens, Value** result);
+
+bool allnumeric(Value* xs)
+{
+    for (;;) {
+        if (isvoid(xs)) {
+            break;
+        }
+        if (!ispair(xs) || !isnumber(xs->p.car)) {
+            return false;
+        }
+        xs = xs->p.cdr;
+    }
+    return true;
+}
+
+size_t countargs(Value* xs) {
+    size_t result = 0;
+    for (Value* c = xs; ispair(c); c = c->p.cdr) {
+        ++result;
+    }
+    return result;
+}
+
+Value* builtin_plus(Value* args) {
+    if (!allnumeric(args)) {
+        return mkstring("error: invalid arguments to <procedure:+>");
+    }
+    double result = 0.;
+    for (Value* c = args; ispair(c); c = c->p.cdr) {
+        assert(isnumber(c->p.car));
+        result += c->p.car->num;
+    }
+    return mknumber(result);
+}
+
+Value* builtin_minus(Value* args) {
+    if (!allnumeric(args)) {
+        return mkstring("error: invalid arguments to <procedure:->");
+    }
+    if (isvoid(args)) {
+        return mknumber(0.);
+    }
+    if (countargs(args) == 1) {
+        return mknumber(-1 * args->p.car->num);
+    }
+    assert(isnumber(args->p.car));
+    double result = args->p.car->num;
+    for (Value* c = args->p.cdr; ispair(c); c = c->p.cdr) {
+        assert(isnumber(c->p.car));
+        result -= c->p.car->num;
+    }
+    return mknumber(result);
+}
+
+Status readsexpr(Tokens& tokens, Value** result)
+{
+    std::vector<Value*> stk;
+    while (!tokens.match(Token::RPAREN)) {
+        if (tokens.match({ Token::FINISHED, Token::ERROR })) {
+            *result = mkstring("missing ')'");
+            return Status::ERROR;
+        }
+        Status ok = eval(tokens, result);
+        if (ok != Status::OK) {
+            return Status::ERROR;
+        }
+        stk.push_back(*result);
+    }
+
+    Value* sexpr = mklist(stk);
+    if (!ispair(sexpr)) {
+        *result = mkstring("s-expression didn't yield a pair!");
+        return Status::ERROR;
+    }
+
+    Value* fn = sexpr->p.car;
+    sexpr = sexpr->p.cdr;
+    if (!ispair(sexpr) && !isvoid(sexpr)) {
+        *result = mkstring("invalid arguments list");
+        return Status::ERROR;
+    }
+
+    if (isbuiltin(fn)) {
+        switch (fn->proc.builtin) {
+            case F_PLUS:
+                *result = builtin_plus(sexpr);
+                return Status::OK;
+            case F_MINUS:
+                *result = builtin_minus(sexpr);
+                return Status::OK;
+        }
+        *result = mkstring("invalid builtin"); return Status::ERROR;
+    } else {
+        std::stringstream ss;
+        ss << "unable to evaluate car of s-expression: " << *fn;
+        *result = mkstring(ss.str());
+        return Status::ERROR;
+    }
+
+    return Status::OK;
+}
+
+Status eval(Tokens& tokens, Value** result)
+{
+    Value* value = tokens.peek();
+    if (tokens.match(Token::FINISHED)) {
+        return Status::DONE;
+    } else if (tokens.match({
+                Token::NUMBER,
+                Token::BOOL,
+                Token::CHAR,
+                Token::STRING,
+                Token::SYMBOL,
+                })) {
+        *result = new Value(*value);
+        return Status::OK;
+    } else if (tokens.match(Token::PLUS)) {
+        *result = mkbuiltin("+", F_PLUS);
+        return Status::OK;
+    } else if (tokens.match(Token::MINUS)) {
+        *result = mkbuiltin("-", F_MINUS);
+        return Status::OK;
+    } else if (tokens.match(Token::LPAREN)) {
+        return readsexpr(tokens, result);
+    } else {
+        *result = mkstring("invalid input");
+        return Status::ERROR;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    if (argc != 2) {
+        printf ("usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "rb");
+    if (!file) {
+        printf("error: cannot open file: %s\n", argv[1]);
+        return 1;
+    }
+
+    Tokens tokens(file);
+    tokens.next();
+    Value* val = nullptr;
+    for (;;) {
+        auto ok = eval(tokens, &val);
+        if (ok == DONE) {
+            break;
+        } else if (ok == ERROR) {
+            if (val) {
+                std::cout << *val << std::endl;
+            }
+            fprintf(stderr, "error: failed to evaluate input!");
+            break;
+        }
+        std::cout << "> " << *val << std::endl;
+    }
+    printf("\n");
+
+    fclose(file);
+
+    return 0;
+}
--- src/cscheme.cpp
+++ src/cscheme.cpp
@@ -1,251 +1,92 @@
 #include <cstdio>
+#include <cstdlib>
+#include <cstdint>
 #include <cassert>
-#include <list>
-#include <vector>
-#include <sstream>
-#include <initializer_list>
-#include "value.h" // TODO: this is evil, must be included _before_ lex.cpp
-#include "lex.cpp"
-
-enum Status { OK, ERROR, DONE };
+#include <unordered_set>
+#include <memory>
 
 enum
 {
-    F_PLUS,
-    F_MINUS,
-    NUM_BUILTINS,
+    TAG_NUM = 0x0,
+    TAG_NIL = 0x1,
+    TAG_FUN = 0x2,
+    TAG_SYM = 0x3,
+    TAG_STR = 0x4,
+
+    TAG_MSK = 0x7,
+    TAG_BITS = 3,
 };
 
-struct Tokens
-{
-    Tokens(FILE* f) noexcept : input(f), token(Token::ERROR) {}
-
-    void next()
-    {
-        if (token == Token::FINISHED) {
-            return;
-        }
-        token = lex(input, &value);
-        if (token == Token::ERROR) {
-            throw std::runtime_error{"error parsing input"};
-        }
-    }
-
-    void expect(Token t)
-    {
-        if (token != t) {
-            // TODO: need printf-style formatter for exceptions
-            throw std::runtime_error{"unexpected token"};
-        }
-        next();
-    }
-
-    bool match(std::initializer_list<Token> tokens)
-    {
-        for (auto t : tokens) {
-            if (match(t)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool match(Token t)
-    {
-        if (token == t) {
-            next();
-            return true;
-        }
-        return false;
-    }
-
-    Value* peek() {
-        return value;
-    }
-
-    Input  input;
-    Token  token;
-    Value* value;
+const char* const TagStr[] = {
+    "NUMBER",
+    "NIL",
+    "FUNCTION",
+    "SYMBOL",
+    "STRING",
 };
 
-Value* mklist(const std::vector<Value*>& values)
+const char* tagtostr(size_t tag) {
+    if (tag > TAG_STR) {
+        return "INVALID TAG";
+    }
+    return TagStr[tag];
+}
+
+enum
 {
-    if (values.empty()) {
-        return NIL;
-    }
-    auto it = values.begin();
-    Value* result = mkpair(*it++, NIL);
-    Value* cur = result;
-    for (; it != values.end(); ++it) {
-        cur->p.cdr = mkpair(*it, NIL);
-        cur = cur->p.cdr;
-    }
-    return result;
-}
+    // function keywords
+    F_DEFINE, F_IF, F_PLUS, F_MINUS,
 
-Status eval(Tokens& tokens, Value** result);
+    // tokens
+    T_DOT, T_LPAREN, T_RPAREN, T_SYM, T_NUM, T_QUOTE, Q_DQUOTE,
+};
 
-bool allnumeric(Value* xs)
+using s64 = int64_t;
+using u64 = uint64_t;
+using Value = u64;
+
+// TODO: intern strings
+std::unordered_set<std::string> strtab;
+const std::string* strintern(std::string s)
 {
-    for (;;) {
-        if (isvoid(xs)) {
-            break;
-        }
-        if (!ispair(xs) || !isnumber(xs->p.car)) {
-            return false;
-        }
-        xs = xs->p.cdr;
-    }
-    return true;
+    auto result = strtab.emplace(std::move(s));
+    auto iterator = result.first;
+    return &*iterator;
 }
+u64 tou64(const void* v) { return reinterpret_cast<u64>(v); }
 
-size_t countargs(Value* xs) {
-    size_t result = 0;
-    for (Value* c = xs; ispair(c); c = c->p.cdr) {
-        ++result;
-    }
-    return result;
-}
-
-Value* builtin_plus(Value* args) {
-    if (!allnumeric(args)) {
-        return mkstring("error: invalid arguments to <procedure:+>");
-    }
-    double result = 0.;
-    for (Value* c = args; ispair(c); c = c->p.cdr) {
-        assert(isnumber(c->p.car));
-        result += c->p.car->num;
-    }
-    return mknumber(result);
-}
-
-Value* builtin_minus(Value* args) {
-    if (!allnumeric(args)) {
-        return mkstring("error: invalid arguments to <procedure:->");
-    }
-    if (isvoid(args)) {
-        return mknumber(0.);
-    }
-    if (countargs(args) == 1) {
-        return mknumber(-1 * args->p.car->num);
-    }
-    assert(isnumber(args->p.car));
-    double result = args->p.car->num;
-    for (Value* c = args->p.cdr; ispair(c); c = c->p.cdr) {
-        assert(isnumber(c->p.car));
-        result -= c->p.car->num;
-    }
-    return mknumber(result);
-}
-
-Status readsexpr(Tokens& tokens, Value** result)
+void dump_strtab()
 {
-    std::vector<Value*> stk;
-    while (!tokens.match(Token::RPAREN)) {
-        if (tokens.match({ Token::FINISHED, Token::ERROR })) {
-            *result = mkstring("missing ')'");
-            return Status::ERROR;
-        }
-        Status ok = eval(tokens, result);
-        if (ok != Status::OK) {
-            return Status::ERROR;
-        }
-        stk.push_back(*result);
+    printf("DUMPING STRING TABLE:\n");
+    for (auto&& s : strtab) {
+        printf("%s\n", s.c_str());
     }
-
-    Value* sexpr = mklist(stk);
-    if (!ispair(sexpr)) {
-        *result = mkstring("s-expression didn't yield a pair!");
-        return Status::ERROR;
-    }
-
-    Value* fn = sexpr->p.car;
-    sexpr = sexpr->p.cdr;
-    if (!ispair(sexpr) && !isvoid(sexpr)) {
-        *result = mkstring("invalid arguments list");
-        return Status::ERROR;
-    }
-
-    if (isbuiltin(fn)) {
-        switch (fn->proc.builtin) {
-            case F_PLUS:
-                *result = builtin_plus(sexpr);
-                return Status::OK;
-            case F_MINUS:
-                *result = builtin_minus(sexpr);
-                return Status::OK;
-        }
-        *result = mkstring("invalid builtin"); return Status::ERROR;
-    } else {
-        std::stringstream ss;
-        ss << "unable to evaluate car of s-expression: " << *fn;
-        *result = mkstring(ss.str());
-        return Status::ERROR;
-    }
-
-    return Status::OK;
+    printf("END STRING TABLE.\n");
 }
 
-Status eval(Tokens& tokens, Value** result)
-{
-    Value* value = tokens.peek();
-    if (tokens.match(Token::FINISHED)) {
-        return Status::DONE;
-    } else if (tokens.match({
-                Token::NUMBER,
-                Token::BOOL,
-                Token::CHAR,
-                Token::STRING,
-                Token::SYMBOL,
-                })) {
-        *result = new Value(*value);
-        return Status::OK;
-    } else if (tokens.match(Token::PLUS)) {
-        *result = mkbuiltin("+", F_PLUS);
-        return Status::OK;
-    } else if (tokens.match(Token::MINUS)) {
-        *result = mkbuiltin("-", F_MINUS);
-        return Status::OK;
-    } else if (tokens.match(Token::LPAREN)) {
-        return readsexpr(tokens, result);
-    } else {
-        *result = mkstring("invalid input");
-        return Status::ERROR;
-    }
-}
+// TODO: make sure sign bit is saved
+Value mknum(int64_t v) { return (v << TAG_BITS) | TAG_NUM; }
+Value mknil() { return TAG_NIL; }
+Value mkfun(int f) { return (f << TAG_BITS) | TAG_FUN; }
+Value mksym(const char* s) { return (tou64(strintern(s)) << TAG_BITS) | TAG_SYM; }
+Value mkstr(const char* s) { return (tou64(strintern(s)) << TAG_BITS) | TAG_STR; }
+
+int64_t totag(Value v) { return v & TAG_MSK; }
+int64_t tonum(Value v) { assert(totag(v) == TAG_NUM); return v >> TAG_BITS; }
+const std::string& tostr(Value v) { assert(totag(v) == TAG_STR); return *(std::string*)(v >> TAG_BITS); }
+const std::string& tosym(Value v) { assert(totag(v) == TAG_SYM); return *(std::string*)(v >> TAG_BITS); }
 
 int main(int argc, char** argv)
 {
-    if (argc != 2) {
-        printf ("usage: %s <filename>\n", argv[0]);
-        return 1;
-    }
+    Value a = mkstr("Hello, World");
+    Value b = mkstr("Hello, World");
+    dump_strtab();
+    printf("a == b? %s\n", a == b ? "TRUE" : "FALSE");
 
-    FILE *file = fopen(argv[1], "rb");
-    if (!file) {
-        printf("error: cannot open file: %s\n", argv[1]);
-        return 1;
-    }
+    printf("A's type: %s\n", tagtostr(totag(a)));
+    printf("B's type: %s\n", tagtostr(totag(b)));
 
-    Tokens tokens(file);
-    tokens.next();
-    Value* val = nullptr;
-    for (;;) {
-        auto ok = eval(tokens, &val);
-        if (ok == DONE) {
-            break;
-        } else if (ok == ERROR) {
-            if (val) {
-                std::cout << *val << std::endl;
-            }
-            fprintf(stderr, "error: failed to evaluate input!");
-            break;
-        }
-        std::cout << "> " << *val << std::endl;
-    }
-    printf("\n");
-
-    fclose(file);
+    printf("Value of a: '%s'\n", tostr(a).c_str());
 
     return 0;
 }
